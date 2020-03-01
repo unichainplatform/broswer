e3077657ceb2fd8ae2087ab6c5a4ef15bf0ecffe
syslink <oursyslink@163.com>
2020-03-01 17:28:02 +0800

    modify first page

--- src/layouts/BasicLayout/BasicLayout.jsx
+++ src/layouts/BasicLayout/BasicLayout.jsx
@@ -263,7 +263,7 @@ class BasicLayout extends Component {
               })}
 
               {/* 首页默认重定向到 /contractDev */}
-              <Redirect exact from="/" to="/contractDev" />
+              <Redirect exact from="/" to="/dashboard" />
             </Switch>
           </Layout.Main>
         </Layout.Section>
